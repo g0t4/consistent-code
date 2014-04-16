bdaa81c370454c7b7e44d0bcc7325863f162ee26
Wesley McClure <wes.mcclure@gmail.com>
2014-04-15 22:33:02 -0700

    Sample of code to cleanup

---
+++ ConsistentCode/ConsistentCode/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsistentCode{public class Class1
+    {public Class1()
+	   {int a = 1;
+	   var b=2;int c=b+a;
+
+
+		    Console.   WriteLine("a + b = " +c    );}}}
